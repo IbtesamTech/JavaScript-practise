3d0f59f6454d35aad3466a644af18f61141339f9
Ibtesam Ahmed ( SK ) <51031949+SkyLineProgrammers@users.noreply.github.com>
2021-11-08 18:52:11 +0500

    matrixElementsSum & allLongestStrings

--- main.js
+++ main.js
@@ -21,3 +21,33 @@ function shapeAreaCount(n) {
   }
   alert(sphareCount);
 }
+
+function matrixElementsSum(matrix) {
+  let sum = 0;
+  for (var i = 0; i < matrix.length; i++) {
+    for (var j = 0; j < matrix[0].length; j++) {
+      if (matrix[i][j] == 0 && i + 1 < matrix.length) {
+        matrix[i + 1][j] = 0;
+      }
+      sum += matrix[i][j];
+    }
+  }
+  alert(sum);
+}
+
+function allLongestStrings(inputArray) {
+  var longestLength = 0;
+  var resArray = [];
+  for (let loop = 0; loop < inputArray.length; loop++) {
+    if (inputArray[loop].length > longestLength) {
+      longestLength = inputArray[loop].length;
+    }
+  }
+  for (let loop = 0; loop < inputArray.length; loop++) {
+    if (inputArray[loop].length == longestLength) {
+      resArray.push(inputArray[loop]);
+    }
+  }
+
+  console.log(resArray);
+}
